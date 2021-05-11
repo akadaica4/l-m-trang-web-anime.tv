f46446ab9e9364e4af0c2b2e1a7832988222ae57
akadaica4 <82006138+akadaica4@users.noreply.github.com>
2021-05-11 10:04:56 +0700

    phần tiếp theo

--- java.js
+++ java.js
@@ -1,8 +1,17 @@
 let counter = 1;
-setInterval(function(){
+setInterval(function() {
     document.getElementById('radio' + counter).checked = true;
     counter++;
-    if (counter > 4){
+    if (counter > 3) {
         counter = 1;
     }
 }, 3000);
+
+
+function trang1() {
+    document.getElementById("pagination1").style.display = "none";
+}
+
+function trang2() {
+    document.getElementById("pagination2").style.visibility = "hidden";
+}
